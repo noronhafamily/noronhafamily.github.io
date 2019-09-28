b45d739d02972e3d1395e8ffaea3304dfa2db00d
Josh <jbn0923@gmail.com>
2019-09-28 11:29:08 -0400

    .

--- pythonprograms/madlibs.py
+++ pythonprograms/madlibs.py
@@ -1,3 +1,8 @@
+name1 = input("type a name   ")
+place1 = input('type a place   ')
+noun1 = input('type a noun   ')
+noun2 = input("type another noun   ")
+place2 = input("type another place   ")
 
 
 
@@ -6,6 +11,5 @@
 
 
 
-
-print("One day,",name1,"was walking down the",place1,"and met a ",noun1,".",name1,"ran away the",noun1,"said,'Hi,my name is Mrs.",noun1,"and i am very hungry!'.So ",name1,"let Mrs.",noun1,"come to ",place2,",and eat some ",noun2,".And they lived happily ever after.")
-#name1   place1   noun1   noun2   place
+print("One day,",name1,"was walking down the",place1,"and met a ",noun1,".",name1,"ran away and the",noun1,"said,'Hi,my name is Mrs.",noun1,"and i am very hungry!'.So ",name1,"let Mrs.",noun1,"come to ",place2,",and eat some ",noun2,".And they lived happily ever after.")
+#name1   place1   noun1   noun2   place2
